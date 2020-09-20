00dd62cfb6c5f053e0765b58bc598b88353d97b7
ROHIT KUMAR <sumitkumar@rohit-2.local>
2020-09-20 23:42:13 +0530

    Add process.env

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cookiePrser = require('cookie-parser');
 const path = require("path");
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 const expressLayouts = require("express-ejs-layouts");
 const db = require('./config/mysql');
 
